223ca7822804b8673f54c41098c93c6d09103d68
TemoPkhakadze <temopkhakadze2002@gmail.com>
2024-06-16 22:43:17 +0400

    Division of players into teams functionality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2
+from team_assigner import TeamAssigner
 
 
 def main():
@@ -10,6 +12,31 @@ def main():
     tracker = Tracker('models/best.pt')
 
     tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pk1')
+
+    # # Save cropped image of a player
+    # for track_id, player in tracks['players'][0].items():
+    #     bbox = player['bbox']
+    #     frame = video_frames[0]
+    #
+    #     # Crop bbox from frame
+    #     cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+    #
+    #     # Save the cropped image
+    #     cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
+    #     break
+
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0],
+                                    tracks['players'][0])
+
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
     # Draw output
     ## Draw object Tracks
